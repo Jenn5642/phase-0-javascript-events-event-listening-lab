194c8cdd6f154b77ea5bddf9b061c4a118cfc2a4
Jenn Robbins <Jennrobbins3@gmail.com>
2022-03-02 22:02:50 -0500

    commit tree 65aea576fa015abe5b7b5d2019fe167f01dfe029

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-let input = document.getElementById('input')
+//let input = document.getElementById('input')
 
-function addingEventListener(){
-  input.addEventListener('click', function() {
-    alert('I was clicked!');
-  });
+//function addingEventListener(){
+  //input.addEventListener('click', function() {
+    //alert('I was clicked!');
+  //});
+//}
+
+const input = document.getElementById('input');
+function addingEventListener() {
+  input.addEventListener('click', addingEventListener);{
+  alert('I was clicked!');
 }
+}
+
